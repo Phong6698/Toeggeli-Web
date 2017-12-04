a74bb93ce057fe770453716d7fbe1e3b8d07b5ea
UEX7852 <chiramet.penglerd@raiffeisen.ch>
2017-12-04 10:32:15 +0100

    Eyyyyyy bootstrap is pretty good

--- src/app/new-game/new-game.component.ts
+++ src/app/new-game/new-game.component.ts
@@ -7,6 +7,19 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class NewGameComponent implements OnInit {
+
+  players = [
+    {name: 'Player1'},
+    {name: 'Player2'},
+    {name: 'Player3'},
+    {name: 'Player4'},
+    {name: 'Player5'},
+    {name: 'Player6'},
+    {name: 'Player7'},
+    {name: 'Player8'},
+    {name: 'Player9'},
+  ]
+
   ngOnInit(): void {
   }
 
